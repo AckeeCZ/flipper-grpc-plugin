c10d37fc064ea28596684833d493db71120b54b3
David Bilik <david.bilik@ackee.cz>
2020-08-10 16:02:07 +0200

    ✨ Extract anonymous classes to named classes

--- android/library/src/main/java/com/ackee/flipper/grpc/FlipperGrpcInterceptor.kt
+++ android/library/src/main/java/com/ackee/flipper/grpc/FlipperGrpcInterceptor.kt
@@ -1,8 +1,16 @@
 package com.ackee.flipper.grpc
 
 import com.google.gson.GsonBuilder
-import io.grpc.*
-import java.util.*
+import io.grpc.CallOptions
+import io.grpc.Channel
+import io.grpc.ClientCall
+import io.grpc.ClientInterceptor
+import io.grpc.ForwardingClientCall
+import io.grpc.ForwardingClientCallListener
+import io.grpc.Metadata
+import io.grpc.MethodDescriptor
+import io.grpc.Status
+import java.util.UUID
 
 /**
  * Interceptor that logs requests and responses from gRPC server
@@ -16,69 +24,77 @@ internal class FlipperGrpcInterceptor(
         callOptions: CallOptions,
         next: Channel
     ): ClientCall<ReqT, RespT> {
-        val id = UUID.randomUUID().toString()
+        val requestId = UUID.randomUUID().toString()
+        return LoggingForwardingClientCall(flipperGrpcPlugin, requestId, method, next, callOptions)
+    }
+}
 
-        return object : ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(
-            next.newCall(
-                method,
-                callOptions
+private class LoggingForwardingClientCall<ReqT, RespT>(
+    private val flipperGrpcPlugin: FlipperGrpcPlugin,
+    private val requestId: String,
+    private val method: MethodDescriptor<ReqT, RespT>,
+    private val next: Channel,
+    callOptions: CallOptions
+) : ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(
+    method,
+    callOptions
+)) {
+
+    private var headers: Metadata? = null
+
+    override fun start(responseListener: ClientCall.Listener<RespT>, headers: Metadata) {
+        this.headers = headers
+        super.start(LoggingClientCallListener(flipperGrpcPlugin, requestId, responseListener), headers)
+    }
+
+    override fun sendMessage(message: ReqT) {
+        super.sendMessage(message)
+        flipperGrpcPlugin.reportRequest(
+            Request(
+                id = requestId,
+                authority = next.authority(),
+                method = method.fullMethodName,
+                data = message?.toJson() ?: "",
+                unixTimestampMs = System.currentTimeMillis(),
+                headers = headers?.toHeaders().orEmpty()
             )
-        ) {
+        )
+    }
+}
 
-            private var headers: Metadata? = null
+private class LoggingClientCallListener<RespT>(
+    private val flipperGrpcPlugin: FlipperGrpcPlugin,
+    private val requestId: String,
+    responseListener: ClientCall.Listener<RespT>
+) : ForwardingClientCallListener.SimpleForwardingClientCallListener<RespT>(
+    responseListener
+) {
 
-            override fun start(responseListener: ClientCall.Listener<RespT>, headers: Metadata) {
-                this.headers = headers
-                class LoggingClientCallListener :
-                    ForwardingClientCallListener.SimpleForwardingClientCallListener<RespT>(
-                        responseListener
-                    ) {
+    private var headers: Metadata? = null
+    private var message: RespT? = null
 
-                    private var headers: Metadata? = null
-                    private var message: RespT? = null
+    override fun onClose(status: Status, trailers: Metadata) {
+        super.onClose(status, trailers)
+        flipperGrpcPlugin.reportResponse(
+            Response(
+                id = requestId,
+                unixTimestampMs = System.currentTimeMillis(),
+                status = status.code.toString(),
+                cause = status.description,
+                headers = (this.headers ?: trailers).toHeaders(),
+                data = message?.toJson()
+            )
+        )
+    }
 
-                    override fun onClose(status: Status, trailers: Metadata) {
-                        super.onClose(status, trailers)
-                        flipperGrpcPlugin.reportResponse(
-                            Response(
-                                id = id,
-                                unixTimestampMs = System.currentTimeMillis(),
-                                status = status.code.toString(),
-                                cause = status.description,
-                                headers = (this.headers ?: trailers).toHeaders(),
-                                data = message?.toJson()
-                            )
-                        )
-                    }
+    override fun onMessage(message: RespT) {
+        super.onMessage(message)
+        this.message = message
+    }
 
-                    override fun onMessage(message: RespT) {
-                        super.onMessage(message)
-                        this.message = message
-
-                    }
-
-                    override fun onHeaders(headers: Metadata?) {
-                        super.onHeaders(headers)
-                        this.headers = headers
-                    }
-                }
-                super.start(LoggingClientCallListener(), headers)
-            }
-
-            override fun sendMessage(message: ReqT) {
-                super.sendMessage(message)
-                flipperGrpcPlugin.reportRequest(
-                    Request(
-                        id = id,
-                        authority = next.authority(),
-                        method = method.fullMethodName,
-                        data = message?.toJson() ?: "",
-                        unixTimestampMs = System.currentTimeMillis(),
-                        headers = headers?.toHeaders().orEmpty()
-                    )
-                )
-            }
-        }
+    override fun onHeaders(headers: Metadata?) {
+        super.onHeaders(headers)
+        this.headers = headers
     }
 }
 
